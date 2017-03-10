b008719c935a327b0ca3ebb990df7d2d03c5a3f6
Cameron Fabbri <cameronfabbri@gmail.com>
2017-03-10 01:12:31 -0600

    delet

--- config.py
+++
@@ -1,6 +0,0 @@
-checkpoint_dir = 'checkpoints/'
-dataset        = 'celeba'
-data_dir       = '/home/fabbric/data/images/celeba/original/'
-learning_rate  = 1e-4
-batch_size     = 64
-multi_gpu      = True
